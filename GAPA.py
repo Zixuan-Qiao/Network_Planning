bf12d68ce38ef72520f01d63144c48a41fd7135c
Zixuan-Qiao <102449059+Zixuan-Qiao@users.noreply.github.com>
2024-01-26 10:37:54 -0500

    Update GAPA.py

--- GAPA.py
+++ GAPA.py
@@ -1,19 +1,15 @@
-import random, math
-from KMCA_complete import KMCA, result_interpreter
+import random
+from parameter_setting import K
+from parameter_setting import V
+from chromosome_metrics import KMCA, result_interpreter
 # from metrics_only_assignment import MOA, result_interpreter
-import sys, getopt
+
 POPULATION_SIZE = 30 # = TOP + MID + LOW
 TOP_SIZE = 10
 MID_SIZE = 10
 LOW_SIZE = 10
 
-ITERATION = 500
-K = 0
-V = 0
-
-# by default, this file implements GAPA and KMCA is imported to calculate metric 
-# when MOA is utilized to calculate metric, this file implements MOP
-
+ITERATION = 20
 
 def random_placement(size):
 
@@ -24,18 +20,30 @@ def random_placement(size):
 
     return placement_set
 
+# print(random_placement())
+
+'''
+def chromosome_metrics(placement):
+
+    metrics = 0
+    for i in range(0, len(placement)):
+        metrics += 10**i * placement[i]
+
+    return metrics
+
+# print(chromosome_metrics((9, 1, 1, 1)))
+'''
 
 def population_generation(size):
 
     population = []
     placement_list = list(random_placement(size))
     for i in range(0, size):
-        metrics = KMCA(placement_list[i], K, V)
-        # metrics = MOA(placement_list[i], K, V)
+        metrics = KMCA(placement_list[i])
+        # metrics = MOA(placement_list[i])
         population.append({"placement": list(placement_list[i]), "metrics": metrics})
 
     return population
-
 
 def population_ranking(population):
 
@@ -47,7 +55,7 @@ def population_ranking(population):
         high = boundry_stack.pop()
         low = boundry_stack.pop()
 
-        if high <= low:
+        if high <= low: # the current sub-array only contain 1 item or is empty
             continue
 
         base = population[low]
@@ -77,7 +85,6 @@ def population_ranking(population):
 
     return population
 
-
 def cross_over(mother, father):
 
     if K % 2 != 0:
@@ -85,11 +92,9 @@ def cross_over(mother, father):
     else:
         boundry = int(K / 2)
 
-    child1 = mother[0:boundry] + father[boundry:]
-    child2 = father[0:boundry] + mother[boundry:]
+    child = mother[0:boundry] + father[boundry:]
 
-    return child1, child2
-
+    return child
 
 def mutation(crossover):
 
@@ -109,13 +114,12 @@ def mutation(crossover):
 
     return crossover
 
-
 def genetic_algorithm():
 
     population = population_generation(POPULATION_SIZE)
     population = population_ranking(population)
-    log_file.write("Initial population: " + str(population) +"\n")
-    summary_file.write("Improve from " + str(population[0]["metrics"])+ " to ")
+    print("Initial population: ", population)
+
     for i in range(0, ITERATION):
         top_p = population[0:TOP_SIZE]
         mid_p = population[TOP_SIZE:TOP_SIZE+MID_SIZE]
@@ -126,79 +130,45 @@ def genetic_algorithm():
             mother = random.sample(top_p, 1)
             father = random.sample(mid_p, 1)
 
-            crossover1, crossover2 = cross_over(mother[0]["placement"], father[0]["placement"])
-            mutated1 = mutation(crossover1)
-            mutated2 = mutation(crossover2)
-            metrics1 = KMCA(mutated1, K, V)
-            metrics2 = KMCA(mutated2, K, V)
-            # metrics1 = MOA(mutated1, K, V)
-            # metrics2 = MOA(mutated2, K, V)
+            crossover = cross_over(mother[0]["placement"], father[0]["placement"])
+            mutated = mutation(crossover)
+            metrics = KMCA(mutated)
+            # metrics = MOA(mutated)
 
-            crossover_children.append({"placement": mutated1, "metrics": metrics1})
-            crossover_children.append({"placement": mutated2, "metrics": metrics2})
+            crossover_children.append({"placement": mutated, "metrics": metrics})
+
         random_children = population_generation(LOW_SIZE)
 
         population = top_p + crossover_children + random_children
         population = population_ranking(population)
-        log_file.write("iteration " + str(i) + ": " + str(population) +"\n")
 
-    log_file.write("Updated population: " +str(population) +"\n")
+    print("Updated population: ", population)
     min_child = population[0]
-    log_file.write("min: " +str(population[0])+ "\n")
-    summary_file.write(str(population[0]["metrics"]) + "\n")
+
     return min_child
 
-if __name__ == '__main__':
-    log_file_name = "output"
-    summary_file_name = "summary_output"
-    opts, args = getopt.getopt(sys.argv[1:],"hk:v:o:s:",["controllers=","forwarding_devices=","output=","summary="])
-    for opt,arg in opts:
-        if opt == "-h":
-            print("Run this with --controllers=<K> --forwarding_devices=<V> --output=<name_of_output_file>")
-            exit()
-        elif opt in ("-k", "--controllers"):
-            K = int(arg)
-        elif opt in ("-v", "--forwarding_devices"):
-            V = int(arg)
-        elif opt in ("-o", "--output"):
-            log_file_name = arg
-        elif opt in ("-s", "--summary"):
-            summary_file_name = arg
-    log_file = open(log_file_name, 'w')
-    summary_file = open(summary_file_name, 'w')
+placement_solution = genetic_algorithm()
 
-    BALANCE_ARRAY = [math.ceil(V/K)] * K
-	
-    placement_solution = genetic_algorithm()
-    
-    print("Placement solution:", placement_solution["placement"])
-    print("Minimum average delay: ", placement_solution["metrics"])
+print("Placement solution:", placement_solution["placement"])
+print("Minimum average delay: ", placement_solution["metrics"])
 
-    # GAPA results output begin
-    # this section needs to be commented when switching to MOP
-    node_assignment = result_interpreter(placement_solution["placement"], K, V)
+# GAPA results output
+node_assignment = result_interpreter(placement_solution["placement"])
 
-    node_count = [0 for i in range(0, K)]
-    for i in range(0, K):
-        node_count[i] = len(node_assignment[i])
+node_count = [0 for i in range(0, K)]
+for i in range(0, K):
+    node_count[i] = len(node_assignment[i])
 
-    print("Node count:", node_count)
+print("Node count:", node_count)
 
-    print("Node assignment solution:")
-    print("Controller index\tAssigned nodes")
-    for i in range(0, K):
-        print(i, "\t", node_assignment[i])
-    # GAPA results output end
-
-    summary_file.close()   
-    log_file.close()
+print("Node assignment solution:")
+print("Controller index\tAssigned nodes")
+for i in range(0, K):
+    print(i, "\t", node_assignment[i])
 
 
-# MOP results output begin
-# this section needs to be commented when switching to GAPA
 '''
-node_count = result_interpreter(placement_solution["placement"], K, V)
+# MOP results output
+node_count = result_interpreter(placement_solution["placement"])
 print(node_count)
 '''
-# MOP results output end
-
